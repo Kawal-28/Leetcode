0689edbd94fc3043505621581607c13654bd6703
Kawal-28 <kawalpreetkaur2810@gmail.com>
2026-08-17 14:11:21 +0530

    Time: 3 ms (57.58%), Space: 44.2 MB (74.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,32 +1,25 @@
 class Solution {
-    // Helper function to check if a StringBuilder is a palindrome
-    private boolean checkPalindrome(StringBuilder str) {
-        int n = str.length();
-        int i = 0;
-        int j = n - 1;
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
 
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j)) {
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            i++;
-            j--;
+
+            left++;
+            right--;
         }
+
         return true;
-    }
-
-    // Main solution function called by LeetCode
-    public boolean isPalindrome(String s) {
-        StringBuilder str = new StringBuilder();
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetterOrDigit(ch)) {
-                str.append(Character.toLowerCase(ch));
-            }
-        }
-
-        return checkPalindrome(str);  
     }
 }
