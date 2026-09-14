79f3af037c470e019d3bce0c384009d00a80e4df
Kawal-28 <kawalpreetkaur2810@gmail.com>
2026-09-14 14:45:00 +0530

    Time: 0 ms (100%), Space: 43.6 MB (69.05%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -14,31 +14,51 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if (root == null) return ans;
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                
-                if (i == size - 1) {
-                    ans.add(node.val);
-                }
-                
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right != null) {
-                    q.add(node.right);
-                }
-            }
+    public void solve(TreeNode root,int depth,List<Integer> ans){
+        //Reverse Preorder as it is right view of binary tree
+        //we have to take right value
+        if(root ==null) return ;
+        if(ans.size()==depth){
+            ans.add(root.val);
         }
+        solve(root.right,depth+1,ans);
+        solve(root.left,depth+1,ans);
+        //return ans;
+    }
+    public List rightSideView(TreeNode root) {
+        List ans = new ArrayList<>();
+        solve(root, 0, ans);
         return ans;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if (root == null) return ans;
+        
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+        
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+            
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode node = q.poll();
+                
+//                 if (i == size - 1) {
+//                     ans.add(node.val);
+//                 }
+                
+//                 if (node.left != null) {
+//                     q.add(node.left);
+//                 }
+//                 if (node.right != null) {
+//                     q.add(node.right);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
